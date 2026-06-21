e772e6c2cb2d8e8f19b24e112881de1fc691cb2d
Ayoub El Abbouchi <elabbouchiayoubenglish@gmail.com>
2026-06-22 00:48:28 +0100

    fix page.js full code

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { auth } from '../firebase';
+import { auth } from './firebase.js';
 import { onAuthStateChanged, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import Link from 'next/link';
 
